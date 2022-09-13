e3a906081cb6949390982363b667d0b8d9a62551
Fatma Aktaş <fatmaaaktass10@gmail.com>
2022-09-13 11:15:00 +0300

    SqutioYazilim en güncel hali

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using SqutioYzlm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SqutioYzlm
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+               name: "Admin",
+               url: "admin/{action}/{id}",
+               defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{action}/{id}",
+                defaults: new { controller = "Home", action = "Anasayfa", id = UrlParameter.Optional }
+            ).RouteHandler = new MyCustomMVCRouteHandler();
+        }
+    }
+}
---
+++ Global.asax.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace SqutioYzlm
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        //protected void Application_BeginRequest()
+        //{
+        //    if (!Request.IsSecureConnection)
+        //    {
+        //        string path = string.Format("https{0}", Request.Url.AbsoluteUri.Substring(4));
+
+        //        Response.Redirect(path);
+        //    }
+        //}
+    }
+}
